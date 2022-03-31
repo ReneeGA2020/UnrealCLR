95dac86676b14fbe3c1c3c474faa24b0d986b7c5
ReneeGA2020 <karazhan@126.com>
2022-03-31 09:47:30 +0800

    test ignore

--- Source/Managed/Tests/TextRenderer.cs
+++ Source/Managed/Tests/TextRenderer.cs
@@ -1,26 +1,30 @@
-namespace UnrealEngine.Tests {
-	public class TextRenderer : ISystem {
-		private Actor actor;
-		private TextRenderComponent textRenderComponent;
-		private Font font;
-		private Material material;
+namespace UnrealEngine.Tests;
 
-		public TextRenderer() {
-			actor = new("TextActor");
-			textRenderComponent = new(actor);
-			font = Font.Load("/Game/Tests/PlayFont");
-			material = Material.Load("/Game/Tests/PlayFontMaterial");
-		}
+public class TextRenderer : ISystem
+{
+    private readonly Actor actor;
+    private readonly TextRenderComponent textRenderComponent;
+    private readonly Font font;
+    private readonly Material material;
 
-		public void OnBeginPlay() {
-			textRenderComponent.SetFont(font);
-			textRenderComponent.SetTextMaterial(material);
-			textRenderComponent.SetText("WELCOME TO THE WORLD");
-			textRenderComponent.SetHorizontalSpacingAdjustment(2.0f);
-			textRenderComponent.SetHorizontalAlignment(HorizontalTextAligment.Center);
-			textRenderComponent.SetVerticalAlignment(VerticalTextAligment.Center);
-			textRenderComponent.SetWorldLocation(new Vector3(200.0f, 0.0f, 0.0f));
-			textRenderComponent.SetWorldRotation(Maths.Euler(0.0f, 0.0f, 180.0f));
-		}
-	}
+    public TextRenderer()
+    {
+        actor = new("TextActor");
+        textRenderComponent = new(actor);
+        font = Font.Load("/Game/Tests/TestFont");
+        material = Material.Load("/Game/Tests/PlayFontMaterial_2");
+    }
+
+    public void OnBeginPlay()
+    {
+        textRenderComponent.SetFont(font);
+        textRenderComponent.SetTextMaterial(material);
+        textRenderComponent.SetText("中文字体测试，这是用C#代码生成的字体");
+        textRenderComponent.SetTextRenderColor(Color.Red);
+        textRenderComponent.SetHorizontalSpacingAdjustment(2.0f);
+        textRenderComponent.SetHorizontalAlignment(HorizontalTextAligment.Center);
+        textRenderComponent.SetVerticalAlignment(VerticalTextAligment.Center);
+        textRenderComponent.SetWorldLocation(new Vector3(200.0f, 0.0f, 0.0f));
+        textRenderComponent.SetWorldRotation(Maths.Euler(0.0f, 0.0f, 180.0f));
+    }
 }
